7edce268ed7fc0142b7a709cf0e31853e009867f
yousef-97 <alshenyousef@gmail.com>
2020-04-26 13:37:17 +0300

    done

--- data/schema.sql
+++ data/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS digimon;
 CREATE TABLE digimon (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    image VARCHAR(255),
+    img VARCHAR(255),
     level VARCHAR(255)
 
 )
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const express = require('express');
 const pg = require('pg');
-const superagent = ('superagent');
+const superagent = require('superagent');
 const methodOverride = require('method-override');
 
 
@@ -18,16 +18,17 @@ const client = new pg.Client(process.env.DATABASE_URL);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static('./public'));
+app.use(express.json())
 app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/', heros);
-app.post('/fav', fav);
-app.get('/save',save);
-app.get('/details/:id',detail);
-app.put('/update/:id',update);
-app.delete('/delete/:id',delete1)
+app.get('/fav', fav);
+app.post('/save', save);
+app.get('/details/:id', detail);
+app.put('/update/:id', update);
+app.delete('/delete/:id', delete1)
 
 
 
@@ -35,12 +36,13 @@ app.delete('/delete/:id',delete1)
 
 function save(req, res) {
     let theFav = req.body;
+    console.log(theFav)
     let SQL = 'INSERT INTO digimon (name,img,level) VALUES ($1,$2,$3);'
-    let safeValues = [theFav.name,theFav.img,theFav.level];
-    client.query(SQL,safeValues)
-    .then(results =>{
-        res.redirect('/fav');
-    })
+    let safeValues = [theFav.name, theFav.img, theFav.level];
+    client.query(SQL, safeValues)
+        .then(results => {
+            res.redirect('/fav');
+        })
 }
 
 function heros(req, res) {
@@ -50,48 +52,53 @@ function heros(req, res) {
 
     superagent.get(url)
         .then(data => {
-            let allheros = data.array.forEach(val => {
+            let allheros = data.body.map(val => {
                 let hero = new Hero(val)
                 return hero;
             });
             res.render('index', { data: allheros })
         })
+        .catch(error=>{
+            erorrHand(req,res,error)
+        })
+        
 }
 function fav(req, res) {
-    
+
     let SQL = 'SELECT * FROM digimon';
     client.query(SQL)
-    .then(results =>{
-        res.render('fav',{data:results.rows})
-    })
+        .then(results => {
+            console.log(results)
+            res.render('fav', { data: results.rows })
+        })
 
 }
 
-function detail(req,res){
+function detail(req, res) {
     let theId = req.params.id;
     let SQL = 'SELECT * FROM digimon WHERE id = $1;';
     safeId = [theId];
-    client.query(SQL,safeId)
-    .then(thedetail=>{
-        res.render('details',{data:thedetail.rows[0]})
-    })
+    client.query(SQL, safeId)
+        .then(thedetail => {
+            res.render('details', { data: thedetail.rows[0] })
+        })
 }
-function update(req,res){
+function update(req, res) {
     theEdit = req.body;
     let SQL = 'UPDATE digimon SET name=$1,img=$2,level=$3 WHERE id=$4;';
-    let safeValues = [theEdit.name,theEdit.img,theEdit.level,req.params.id];
-    client.query(SQL,safeValues)
-    .then(()=>{
-        res.redirect(`/details/${req.params.id}`)
-    })
+    let safeValues = [theEdit.name, theEdit.img, theEdit.level, req.params.id];
+    client.query(SQL, safeValues)
+        .then(() => {
+            res.redirect(`/details/${req.params.id}`)
+        })
 }
-function delete1(req,res){
+function delete1(req, res) {
     let SQL = 'DELETE FROM digimon WHERE id=$1;'
     let safeId = [req.params.id];
-    client.query(SQL,safeId)
-    .then(()=>{
-        res.redirect(`/fav`);
-    })
+    client.query(SQL, safeId)
+        .then(() => {
+            res.redirect(`/fav`);
+        })
 }
 
 
@@ -109,3 +116,8 @@ client.connect()
             console.log((`lestining on ${PORT}`))
         })
     })
+
+function erorrHand(req,res,err){
+    console.log(err)
+}
+
